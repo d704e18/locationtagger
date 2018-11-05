fd3807244e18aaa469e06ec3e34fed4cd2dfda03
Alex <alexnlarsen@gmail.com>
2018-11-05 14:30:57 +0100

    added generator utility function

--- machine_learning_models/utils.py
+++ machine_learning_models/utils.py
@@ -12,6 +12,7 @@ class AttrDict(dict):
 
 class cd:
     """Context manager for changing the current working directory"""
+
     def __init__(self, newPath):
         self.newPath = os.path.expanduser(newPath)
 
@@ -27,7 +28,8 @@ def load_data(file_path, normalize=True, drop_device=True):
     if file_path.endswith('pkl'):
         data = pd.read_pickle(file_path)
     else:
-        data = pd.read_csv(file_path, parse_dates=True, dayfirst=False, index_col=0)
+        data = pd.read_csv(
+            file_path, parse_dates=True, dayfirst=False, index_col=0)
 
     if drop_device and 'Device' in data.columns:
         data = data.drop("Device", axis=1)
@@ -49,7 +51,8 @@ def load_prediction_data(file_path, device_id, transformer):
     if file_path.endswith('pkl'):
         data = pd.read_pickle(file_path)
     else:
-        data = pd.read_csv(file_path, parse_dates=True, dayfirst=False, index_col=0)
+        data = pd.read_csv(
+            file_path, parse_dates=True, dayfirst=False, index_col=0)
     data = data.loc[data['Device'] == device_id]
     timestamps = data.index
     x = data.iloc[:, 0:-2].values.astype(np.float32)
@@ -58,11 +61,67 @@ def load_prediction_data(file_path, device_id, transformer):
     return transformer.transform(x), y, timestamps
 
 
+def get_example_generator(file_path, **kwargs):
+    """
+    Returns three generators;
+    train_gen contains the first 5 days
+    val_gen   contains the second to last day
+    test_gen  contains the last day
+    kwargs    are passed on to load_data_and_split
+    """
+    validation_start = '2018-09-11 10:00'
+    test_start = '2018-09-12 12:00'
+
+    if file_path.endswith('.csv'):
+        df = pd.read_csv(
+            file_path, dayfirst=False, parse_dates=True, index_col=0)
+    elif file_path.endswith('.pkl'):
+        df = pd.read_pickle(file_path)
+    else:
+        raise ValueError("Unknown filetype", file_path)
+
+    areas = df.iloc[:, -1]
+    hot_areas = one_hot(areas)
+    m, n = hot_areas.shape
+    area_names = []
+    for i in range(n):
+        area_name = 'area{}'.format(i)
+        area_names += [area_name]
+        df[area_name] = hot_areas[:, i]
+
+    df = df.drop('areas', axis=1)
+
+    train_df = df[:validation_start]
+    fit_data = train_df.drop('Device', axis=1).drop(area_names, axis=1)
+    validation_df = df[validation_start:test_start]
+    test_df = df[test_start:]
+
+    transformer = StandardScaler().fit(fit_data)
+
+    def get_generator(df):
+        groups = df.groupby('Device')
+        for device, group in groups:
+            person = group.drop('Device', axis=1).sort_index()  # ensure the observations are ordered
+            labels = person[area_names]
+            # labels = labels.values # for production
+            readings = person.drop(area_names, axis=1)
+            # readings = transformer.transform(readings) # for production
+            yield readings, labels#, device
+
+    return get_generator(train_df), \
+        get_generator(validation_df), \
+        get_generator(test_df), \
+        transformer
+
+    # return train_x, train_y, validation_x, validation_y, test_x, test_y
+
+
 def load_data_and_split(file_path, normalize=True, drop_device=True):
     if file_path.endswith('pkl'):
         data = pd.read_pickle(file_path)
     else:
-        data = pd.read_csv(file_path, parse_dates=True, dayfirst=False, index_col=0)
+        data = pd.read_csv(
+            file_path, parse_dates=True, dayfirst=False, index_col=0)
 
     if drop_device and 'Device' in data.columns:
         data = data.drop('Device', axis=1)
@@ -76,8 +135,10 @@ def load_data_and_split(file_path, normalize=True, drop_device=True):
     train_x = data[:train_val_split].iloc[:, 0:-1].values.astype(np.float32)
     train_y = data[:train_val_split].iloc[:, -1:].values.astype(np.int32)
 
-    validation_x = data[train_val_split:val_test_split].iloc[:, 0:-1].values.astype(np.float32)
-    validation_y = data[train_val_split:val_test_split].iloc[:, -1:].values.astype(np.int32)
+    validation_x = data[train_val_split:
+                        val_test_split].iloc[:, 0:-1].values.astype(np.float32)
+    validation_y = data[train_val_split:
+                        val_test_split].iloc[:, -1:].values.astype(np.int32)
 
     test_x = data[val_test_split:].iloc[:, 0:-1].values.astype(np.float32)
     test_y = data[val_test_split:].iloc[:, -1:].values.astype(np.int32)
@@ -92,21 +153,41 @@ def load_data_and_split(file_path, normalize=True, drop_device=True):
     print("Validation shape: {}".format(validation_x.shape))
     print("Test shape: {}".format(test_x.shape))
 
-    return train_x, train_y, validation_x, one_hot(validation_y), test_x, one_hot(test_y), transformer
+    return train_x, train_y, validation_x, one_hot(
+        validation_y), test_x, one_hot(test_y), transformer
 
 
 def one_hot(y):
+    if type(y) is pd.Series:
+        y = y.values.reshape(-1, 1)
     num_classes = np.unique(y).shape[0]
 
-    return np.squeeze(np.eye(num_classes)[y.reshape(-1).astype(int)]).astype(np.int32)
+    return np.squeeze(np.eye(num_classes)[y.reshape(-1).astype(int)]).astype(
+        np.int32)
 
 
 if __name__ == "__main__":
 
     path = os.getcwd()
     parent = '/'.join(path.split('/')[:-1])
+    data_path = parent + "/data/trimmed-aggregated-training-data.csv"
 
-    train_x, train_y, validation_x, validation_y, test_x, test_y = load_data(parent+"/data/whole_week.pkl")
+    # train_x, train_y, validation_x, validation_y, test_x, test_y = load_data(
+    #     parent + "/data/whole_week.pkl")
 
-    print(train_x.dtype)
-    print(train_y.dtype)
+    # print(train_x.dtype)
+    # print(train_y.dtype)
+    g1, g2, g3, t = get_example_generator(data_path)
+
+    for obs, label in g1:
+        pass
+    for obs, label in g2:
+        pass
+    for obs, label in g3:
+        pass
+        # print(obs)
+        # input()
+        # print(label)
+        # input()
+        # print(device)
+        # input()
