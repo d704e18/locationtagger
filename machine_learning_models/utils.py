229438cf18e1c72cf372529c489b45705303f523
OpieOP <alexnlarsen@gmail.com>
2018-11-29 13:25:27 +0100

    added scaling to utils.py generators

--- machine_learning_models/utils.py
+++ machine_learning_models/utils.py
@@ -61,16 +61,32 @@ def load_prediction_data(file_path, device_id, transformer):
     return transformer.transform(x), y, timestamps
 
 
-def get_example_generator(file_path, repeat=True, clip=None,
-                          filter_staff=True):
+def scale_df(df, drop_columns, end1, start2):
+    scalable_df = df.drop(drop_columns, axis=1)
+    transformer = StandardScaler().fit(scalable_df[:end1].append(
+        scalable_df[start2:]))
+
+    scalable_df = pd.DataFrame(
+        transformer.transform(scalable_df),
+        index=scalable_df.index,
+        columns=scalable_df.columns)
+
+    # Keep the unscaled columns without modifying them
+    scalable_df[drop_columns] = df[drop_columns]
+    return scalable_df, transformer
+
+
+def get_example_generator(file_path, repeat=True, clip=None):
     """
     Returns three generators:
     train_gen contains the first 5 days
     val_gen   contains the second to last day
     test_gen  contains the last day
     """
-    validation_start = '2018-09-11 10:00'
-    test_start = '2018-09-12 12:00'
+    # validation_start = '2018-09-11 10:00'
+    # test_start = '2018-09-12 12:00'
+    validation_start = '2018-09-09 18:00'
+    validation_end = '2018-09-10 18:00'
 
     if file_path.endswith('.csv'):
         df = pd.read_csv(
@@ -91,30 +107,30 @@ def get_example_generator(file_path, repeat=True, clip=None,
 
     df = df.drop('areas', axis=1)
 
-    train_df = df[:validation_start]
-    fit_data = train_df.drop('Device', axis=1).drop(area_names, axis=1)
-    validation_df = df[validation_start:test_start]
-    test_df = df[test_start:]
+    # Drop the columns that should not be scaled
+    unscalable_columns = ['Device'] + area_names
+    df, transformer = scale_df(df, unscalable_columns, validation_start,
+                           validation_end)
+    df = df.sort_index()
 
-    transformer = StandardScaler().fit(fit_data)
+    # Train on everything outside of the validation interval
+    train_df = df[:validation_start].append(df[validation_end:])
+    validation_df = df[validation_start:validation_end]
+    # Split validation into test and validation
+    test_df = validation_df.iloc[validation_df.shape[0] // 2:]
+    validation_df = validation_df.iloc[:validation_df.shape[0] // 2]
 
     def get_generator(df):
-        while True:
-            groups = df.groupby('Device')
-            for device, group in groups:
-                person = group.drop(
-                    'Device',
-                    axis=1).sort_index()  # ensure the observations are ordered
-                labels = person[area_names]
-                # labels = labels.values # for production
-                readings = person.drop(area_names, axis=1)
-                # readings = transformer.transform(readings) # for production
-                if clip is not None:
-                    labels = labels[:clip]
-                    readings = readings[:clip]
-                yield readings, labels  # , device
-            if not repeat:
-                break
+        groups = df.groupby('Device')
+        for device, group in groups:
+            person = group.drop('Device', axis=1)
+            labels = person[area_names]
+            labels = labels.values  # for production
+            readings = person.drop(area_names, axis=1)
+            if clip is not None:
+                labels = labels[:clip]
+                readings = readings[:clip]
+            yield readings, labels  # , device
 
     return get_generator(train_df), get_generator(
         validation_df), get_generator(test_df), transformer
